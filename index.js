00103626ca88df5566e2b4dfd17c0712861a7826
Sean Joseph <seandjoseph@gmail.com>
2016-03-17 22:33:02 -0400

    adding some changes

--- index.js
+++ index.js
@@ -13,7 +13,19 @@ server.route({
     handler: function (request, reply) {
         var fs = require('fs');
         var payload = JSON.parse(fs.readFileSync('./testData.json', 'utf8'));
-        var fbiaRssBuilder = new FbiaRssBuilder(payload);
+        var config = {
+            'noVideos': request.params.noVideos|| 0,
+            'embedVideos': request.params.embedVideos || 1,
+            'removeRelateds': request.params.removeRelateds || 1,
+            'preferredVideo': 'ios_1240',
+            'videoCDN': 'http://ht3.cdn.turner.com/cnn/big',
+            'preferredImage': 'exlarge16to9',
+            'inStoryImage': 'mediumplus16to9',
+            'imageCDN': 'http://i2.cdn.turner.com/cnnnext/dam/assets',
+            'contentHost': 'http://www.cnn.com'
+        }
+
+        var fbiaRssBuilder = new FbiaRssBuilder(payload, config);
 
 
         reply(fbiaRssBuilder.getFeed()).header('Content-Type',' text/xml');
